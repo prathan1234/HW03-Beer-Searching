6312e59417f0b1316040f04d532cee611ac76dce
prathan1234 <prathan.psong@gmail.com>
2017-09-06 10:37:58 +0700

    Add searching with 1 field. Cannot search multiple fields.

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,43 +1,58 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 //ES6
 var obj;
-var param = "beer_name";
-var val = "";
+var api;
+var param = [];
+var val = [];
 var api_string = "";
 
-if (param != "" && val != "") {
-    api_string = "?" + param + "=" + val;
+// if (param != "" && val != "") {
+//     api_string += "?" + param + "=" + val;
+// }
+
+setapi();
+
+function setapi() {
+    api = 'https://api.punkapi.com/v2/beers' + api_string;
+    console.log(api);
+
+    var getBeersInformation = function getBeersInformation() {
+        return fetch(api);
+    };
+
+    getBeersInformation().then(function (response) {
+        return response.json();
+    }).catch(function (e) {
+        return console.error('You failed');
+    }).then(function (response) {
+        obj = response;
+        createTable();
+    });
 }
-
-var api = 'https://api.punkapi.com/v2/beers' + api_string;
-console.log(api);
-
-var getBeersInformation = function getBeersInformation() {
-    return fetch(api);
-};
-
-getBeersInformation().then(function (response) {
-    return response.json();
-}).catch(function (e) {
-    return console.error('You failed');
-}).then(function (response) {
-    obj = response;
-    createTable();
-});
 
 function createTable() {
     var txt = "<table border='1px' style='border-collapse: collapse; width: 100%'>";
 
     txt += "<tr style='background-color: #DDDDDD;'>";
     txt += "<th>" + "Beer name" + "</th>";
+    txt += "<th>" + "ABV" + "</th>";
+    txt += "<th>" + "Food pairing" + "</th>";
+    txt += "<th>" + "Yeast" + "</th>";
+    txt += "<th>" + "First brewed" + "</th>";
+    txt += "<th>" + "pH" + "</th>";
     txt += "</tr>";
 
     for (var x = 0; x < obj.length; x++) {
-        console.log(obj[x].name);
+        console.log(obj[x]);
         txt += "<tr>";
         txt += "<td>" + obj[x].name + "</td>";
+        txt += "<td>" + obj[x].abv + "</td>";
+        txt += "<td>" + obj[x].food_pairing + "</td>";
+        txt += "<td>" + obj[x].ingredients.yeast + "</td>";
+        txt += "<td>" + obj[x].first_brewed + "</td>";
+        txt += "<td>" + obj[x].ph + "</td>";
         txt += "</tr>";
     }
 
@@ -62,6 +77,61 @@ function createTable() {
     // xmlhttp.open("POST", "json_demo_db_post.php", true);
     // xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     // xmlhttp.send("x=" + dbParam);
+}
+
+$(document).ready(function () {
+    $(".btn_search").click(function () {
+        onclick();
+    });
+});
+
+function onclick() {
+    var x = 0;
+    var search_name = document.getElementById("beer_name").value;
+    var search_food = document.getElementById("food").value;
+    var search_yeast = document.getElementById("yeast").value;
+
+    if (search_name == "" && search_food == "" && search_yeast == "") {
+        api_string = "";
+    } else {
+        api_string = "";
+        api_string += "?";
+
+        while (true) {
+            if (search_name != "") {
+                param[x] = "beer_name";
+                val[x] = search_name;
+                search_name = "";
+            }
+            if (search_food != "") {
+                param[x] = "food";
+                val[x] = search_food;
+                search_food = "";
+            }
+            if (search_yeast != "") {
+                param[x] = "yeast";
+                val[x] = search_yeast;
+                search_yeast = "";
+            }
+
+            if (x == 0) {
+                api_string += param[x] + "=" + val[x];
+            } else {
+                api_string += "&" + param[x] + "=" + val[x];
+            }
+
+            if (search_name == "" && search_food == "" && search_yeast == "") {
+                break;
+            }
+            x++;
+        }
+    }
+
+    console.log(api_string);
+    setapi();
+    // createTable();
+
+    // https://api.punkapi.com/v2/beers?beer_name=Buzz&food=spicy
 }
 
 },{}]},{},[1]);
--- js/main.js
+++ js/main.js
@@ -1,38 +1,53 @@
 //ES6
 var obj;
-var param = "beer_name";
-var val = "";
+var api;
+var param = [];
+var val = [];
 var api_string = "";
 
-if (param != "" && val != "") {
-    api_string = "?" + param + "=" + val;
-}
+// if (param != "" && val != "") {
+//     api_string += "?" + param + "=" + val;
+// }
 
-var api = 'https://api.punkapi.com/v2/beers' + api_string;
-console.log(api);
+setapi();
 
-const getBeersInformation = () =>
+function setapi() {
+    api = 'https://api.punkapi.com/v2/beers' + api_string;
+    console.log(api);
+
+    const getBeersInformation = () =>
     fetch(api);
 
-getBeersInformation()
-    .then(response => response.json())
-    .catch(e => console.error('You failed'))
-    .then(response => {
+    getBeersInformation()
+        .then(response => response.json())
+        .catch(e => console.error('You failed'))
+        .then(response => {
         obj = response;
         createTable();
     });
+}
 
 function createTable() {
     var txt = "<table border='1px' style='border-collapse: collapse; width: 100%'>";
 
     txt += "<tr style='background-color: #DDDDDD;'>";
     txt += "<th>" + "Beer name" + "</th>";
+    txt += "<th>" + "ABV" + "</th>";
+    txt += "<th>" + "Food pairing" + "</th>";
+    txt += "<th>" + "Yeast" + "</th>";
+    txt += "<th>" + "First brewed" + "</th>";
+    txt += "<th>" + "pH" + "</th>";
     txt += "</tr>";
 
     for (var x = 0; x < obj.length; x++) {
-        console.log(obj[x].name);
+        console.log(obj[x]);
         txt += "<tr>";
         txt += "<td>" + obj[x].name + "</td>";
+        txt += "<td>" + obj[x].abv + "</td>";
+        txt += "<td>" + obj[x].food_pairing + "</td>";
+        txt += "<td>" + obj[x].ingredients.yeast + "</td>";
+        txt += "<td>" + obj[x].first_brewed + "</td>";
+        txt += "<td>" + obj[x].ph + "</td>";
         txt += "</tr>";
     }
 
@@ -57,4 +72,59 @@ function createTable() {
     // xmlhttp.open("POST", "json_demo_db_post.php", true);
     // xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     // xmlhttp.send("x=" + dbParam);
+}
+
+$(document).ready(function () {
+   $(".btn_search").click(function () {
+       onclick();
+   });
+});
+
+function onclick() {
+    var x = 0;
+    var search_name = document.getElementById("beer_name").value;
+    var search_food = document.getElementById("food").value;
+    var search_yeast = document.getElementById("yeast").value;
+
+    if (search_name == "" && search_food == "" && search_yeast == "") {
+        api_string = "";
+    } else {
+        api_string = "";
+        api_string += "?";
+
+        while (true) {
+            if (search_name != "") {
+                param[x] = "beer_name";
+                val[x] = search_name;
+                search_name = "";
+            }
+            if (search_food != "") {
+                param[x] = "food";
+                val[x] = search_food;
+                search_food = "";
+            }
+            if (search_yeast != "") {
+                param[x] = "yeast";
+                val[x] = search_yeast;
+                search_yeast = "";
+            }
+
+            if (x == 0) {
+                api_string += param[x] + "=" + val[x];
+            } else {
+                api_string += "&" + param[x] + "=" + val[x];
+            }
+
+            if (search_name == "" && search_food == "" && search_yeast == "") {
+                break;
+            }
+            x++;
+        }
+    }
+
+    console.log(api_string);
+    setapi();
+    // createTable();
+
+    // https://api.punkapi.com/v2/beers?beer_name=Buzz&food=spicy
 }
